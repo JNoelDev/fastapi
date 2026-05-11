8724d74879416768b26f05b1875d38d868b298e4
Jean Noël <jeannoelp25sesame@gmail.com>
2026-05-11 19:45:35 +0300

    schemas pydantic user

---
+++ app/schemas/user.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel,EmailStr,SecretStr,ConfigDict
+from datetime import datetime
+from fastapi import Form
+from .enums import Sex
+
+
+"""inscription en locale"""
+class UserCreateLocal(BaseModel):
+    username : str
+    email : EmailStr
+    password : SecretStr
+
+    phone : str|None = None
+    sexe : Sex 
+
+    @classmethod
+    def as_form(cls,
+        username : str = Form(...),
+        email : EmailStr = Form(...),
+        password : SecretStr = Form(...),
+        phone : str|None = Form(...),
+        sexe : Sex = Form(default=None)
+    ):
+        
+        return cls(
+            username=username,
+            email=email,password=password,
+            phone=phone,sexe=sexe
+            )
+
+"""information après la verification"""   
+class UserOut(BaseModel):
+    id : int
+    username : str
+    code_user : str
+    verified : bool
+    created_at : datetime
+
+    model_config=ConfigDict(from_attributes=True)
+
+"""Utilisateur vu par recherche"""
+class UserProfile_Public(BaseModel):
+    full_name : str
+    profile_picture : str
+    code_user : str
+    relationship_status : str
+
+    model_config=ConfigDict(from_attributes=True) 
+
+
+
