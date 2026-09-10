77859a87a979ae2061d1aa100daef0e8e8618a26
Jean Noël <jeannoelp25sesame@gmail.com>
2026-09-10 17:37:52 +0300

    update column on env.py

--- alembic/versions/b667279b2226_initial.py
+++ alembic/versions/b667279b2226_initial.py
@@ -5,7 +5,7 @@ Revises:
 Create Date: 2026-09-10 17:11:54.681288
 
 """
-from typing import Sequence, Union
+from typing import Sequence
 
 from alembic import op
 import sqlalchemy as sa
@@ -13,9 +13,9 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'b667279b2226'
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
