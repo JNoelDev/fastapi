dafc76020035711e2e749603ba357a4e6709c43e
Jean Noël <jeannoelp25sesame@gmail.com>
2026-09-13 14:13:38 +0300

    update register_router

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ settings=get_settings()
 
 app=FastAPI(
     title=settings.app_name,
-    docs_url=f"{settings.prefix_app}/docs" if settings.environment=="development" else None,
+    docs_url=f"{settings.prefix_app}/docs",
     redoc_url=f"{settings.prefix_app}/redoc" if settings.environment=="development" else None,
 )
 
